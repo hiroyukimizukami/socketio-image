08cecb4337fcdb7adf55ffdb9b7ff567f6552f40
hiroyuki.mizukami <majestaaa@gmail.com>
2013-11-21 02:28:04 +0900

    init

---
+++ public/javascripts/index.js
@@ -0,0 +1,29 @@
+(function () {
+     var socket = io.connect('http://localhost');
+
+     socket.on('notify', function (data) {
+                   $('#image001').attr('src', data);
+
+               });
+
+     var onSubmit = function (e) {
+         var input = $('#input_image001').get(0);
+         var file = input.files[0];
+         var name = file.name;
+
+         var reader = new FileReader();
+         reader.onloadend = function (e) {
+             var data = e.target.result;
+
+             if (!data) {
+                 return ;
+             }
+
+             socket.emit('upload', { name : name, data : data});
+         };
+
+         reader.readAsDataURL(file);
+     };
+
+     $('#submit001').click(onSubmit);
+})();
